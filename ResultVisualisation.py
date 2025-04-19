d613413dc8d7a68f8be8bd1bfffa9c26cc9b18f9
ngernyi <122040135+ngernyi@users.noreply.github.com>
2025-04-20 05:43:53 +0800

    Delete ResultVisualisation.py

--- ResultVisualisation.py
+++
@@ -1,6 +0,0 @@
-class ResultVisualisation:
-    def __init__(self, backtest):
-        self.backtest = backtest
-    def visualise(self):
-        # 1. visualise the result of the backtest by showing the matrices and graphs
-        pass
