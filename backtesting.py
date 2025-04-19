6f7728e60eb9f69e082c685fad08f8aae4f4e124
ngernyi <122040135+ngernyi@users.noreply.github.com>
2025-04-20 05:44:15 +0800

    Delete backtesting.py

--- backtesting.py
+++
@@ -1,7 +0,0 @@
-class Backtesting:
-    def __init__(self, strategy, data):
-        self.strategy = strategy
-        self.data = data
-    def backtest(self):
-        # 1. do back testing with data from past 3 years using the strategy
-        pass
